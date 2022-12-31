73df935256daef6b4b2cdc95234bca4a138e97ad
Dan Wright <60994655+wrightdj99@users.noreply.github.com>
2022-12-31 15:29:19 -0600

    Fixed rendering

--- src/App.js
+++ src/App.js
@@ -1,28 +1,27 @@
 import './App.css';
 import axios from 'axios';
-import { Component } from 'react';
+import React, {} from 'react';
 
 //Declaring a new class to make a call out to a Random Number Restful API using Axios.
-class App extends Component{
+const baseRequest = 'http://www.randomnumberapi.com/api/v1.0/randomredditnumber?min=1&max=100&count=1';
+function App(){
   //Helper function that actually performs the get request
-  getNumber = () => {
-    axios.get('http://www.randomnumberapi.com/api/v1.0/randomredditnumber?min=1&max=100&count=1')
-    //When retrieved...
-    .then(res => {
-      alert(res.data);
-    }).catch(err => {
-      console.log(err);
-    })
-  }
+    const [number, setNumber] = React.useState(null);
+    React.useEffect(() => {
+        axios.get(baseRequest)
+      //When retrieved...
+      .then(res => {
+        setNumber(res.data);
+        console.log(res);
+      });
+    }, []);
   //Simple rendering. We're not going for a tear-jerkingly beautiful UI, only for a demonstration on how Axios works.
-  render(){
   return (
     <div className="App">
       <h1>Random Axios Number Generator</h1>
-      <button onClick={this.getNumber.bind(this)}>Get Number</button>
+      <h2>{number}</h2>
     </div>
   );
-  }
 }
 
 export default App;
